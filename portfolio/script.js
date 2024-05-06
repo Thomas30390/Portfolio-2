67a2837370af5b9f1427c90f7a93c8335580b72a
Thomas Deverrewaere <deverrewaerethomas@gmail.com>
2024-05-06 17:07:30 +0200

    mise a jour

--- portfolio/script.js
+++ portfolio/script.js
@@ -23,3 +23,13 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll(".image-amovible img");
+    let index = 0;
+
+    setInterval(() => {
+        images.forEach(img => img.classList.remove('active'));
+        images[index].classList.add('active');
+        index = (index + 1) % images.length;
+    }, 3000); // Changez 3000 pour ajuster la durée entre chaque transition (en millisecondes)
+});
